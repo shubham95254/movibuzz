ccdb939f27c3584ec7403d5ef2bd6c78fce054e1
Shubham <125039017+shubham95254@users.noreply.github.com>
2024-01-15 15:47:14 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import { fetchDataFromApi } from './utils/api'
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from './store/homeSlice';
 
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
